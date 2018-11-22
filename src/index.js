f8b44b165f9d1be8fd43c6c5a2eba809e771cf1c
Roman <smakula2012@gmail.com>
2018-11-22 21:52:28 +0200

    add sass

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import './scss/main.scss';
+
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -18,7 +19,20 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
             // ...additional rules...
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
 };
